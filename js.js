f84eba637ce15c2b6ac7844adb895db164591052
George Higbie <gehigbie@gmail.com>
2017-01-15 00:50:49 -0700

    added object and notes

--- js.js
+++ js.js
@@ -12,8 +12,29 @@ var person = {
 	city: "LA"
 }
 
-//we can use bracket notation 
+//we can use bracket notation - similar to arrays
 console.log(person["name"]);
 
 //we can use dot notation
 console.log(person.name);
+
+var dog = {
+	name: "Rusty",
+	breed: "mut",
+	age: 3
+}
+
+console.log(dog.age);
+
+//dot notation cannot be used if the property starts with a number or has a space
+//bracket notation can look for variable names and do notation cannot
+
+//updating object can be down with either bracket notation or dot notation
+dog.age = 4;
+dog.age +=1;
+console.log(dog.age);
+
+dog["name"] = "Tater";
+
+console.log(dog);
+
