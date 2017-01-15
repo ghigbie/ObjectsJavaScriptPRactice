64dde5b3dde69c289d28007468e7e7bdd52cfcdb
George Higbie <gehigbie@gmail.com>
2017-01-15 00:44:28 -0700

    added another object to file

--- js.js
+++ js.js
@@ -1,7 +1,19 @@
 //this doc was created in the terminal
-
+//key value pairs
 var person = {
 	name: "Cindy",
 	age: 32,
 	city: "Missoula"
 };
+//there is no order in an object 
+var person = {
+	name: "Travis",
+	age: 21,
+	city: "LA"
+}
+
+//we can use bracket notation 
+console.log(person["name"]);
+
+//we can use dot notation
+console.log(person.name);
